326524ba16f70103f7109ca915d16f66b5ab7110
sergio-losada <sergio.losada@outlook.com>
2025-06-03 13:19:05 +0200

    Docker compose

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,5 +1,7 @@
-export const databaseHost = 'localhost';
-export const databaseUser = 'root';
-export const databasePort = 9306;
-export const databaseName = 'test';
+export const databaseHost = process.env.DB_HOST || 'localhost';
+export const databaseUser = process.env.DB_USER || 'root';
+export const databasePort = parseInt(process.env.DB_PORT || '3306');
+export const databaseName = process.env.DB_NAME || 'test';
+export const databasePassword = process.env.DB_PASSWORD || '';
+
 export const JWT_SECRET_KEY = 'jwt_secret'
